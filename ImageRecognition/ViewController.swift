0f54dbd5b2cdda09765754c6b708d488c6c1dacd
Artur Pahasian <art@devs.ae>
2021-10-14 12:09:18 +0300

    Xcode project created  .gitignore added

---
+++ ImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by Arthur Poghosyan on 14.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
