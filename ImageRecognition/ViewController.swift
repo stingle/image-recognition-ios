33c3d031457a6fcf4a845b38088e5fd6a8a33c7d
Artur Pahasian <art@devs.ae>
2021-10-19 17:22:32 +0300

    Added functionality of objects detection for selected photo from phone photos

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -6,58 +6,84 @@
 //
 
 import UIKit
-import AVKit
-import Vision
 
-class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var verdictLabel: UILabel!
+    let imagePredictor = ImagePredictor()
+
+    let predictionsToShow = 2
+
+    // MARK: Main storyboard outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let captureSesstion = AVCaptureSession()
-        captureSesstion.sessionPreset = .photo
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-        guard let inptut = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        captureSesstion.addInput(inptut)
-        captureSesstion.startRunning()
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSesstion)
-        self.view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        
-        let dataOutput = AVCaptureVideoDataOutput()
-        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-        captureSesstion.addOutput(dataOutput)
-        
-
+    // MARK: Main storyboard actions
+    @IBAction func singleTap() {
+        present(photoPicker, animated: false)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
-    }
-
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        //todo configure cpu gpu all //natural engine
-        //MobileNet.mlkitmodel
-        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
-        let request = VNCoreMLRequest(model: model) { (finishedReq , err) in
-            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-//            print(results)
-            guard let firsObservation = results.first else { return }
-            let percentOfConfidence = (firsObservation.confidence * 100).rounded() / 100
-//            print(firsObservation.identifier, " ", percentOfConfidence)
-            print(results[0].identifier, (results[0].confidence*100).rounded()/100)
-            print(results[1].identifier, (results[1].confidence*100).rounded()/100)
-            print(results[2].identifier, (results[2].confidence*100).rounded()/100)
-            DispatchQueue.main.async {
-                self.verdictLabel.text = firsObservation.identifier + " " + String(percentOfConfidence)
-            }
-            
+    // MARK: Main storyboard updates
+    func updateImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
         }
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+
+    func updatePredictionLabel(_ message: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = message
+        }
+    }
+
+    func userSelectedPhoto(_ photo: UIImage) {
+        updateImage(photo)
+        updatePredictionLabel("Making predictions for the photo...")
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.classifyImage(photo)
+        }
+    }
+    
+    // MARK: Image prediction methods
+    /// Sends a photo to the Image Predictor to get a prediction of its content.
+    /// - Parameter image: A photo.
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+    /// The method the Image Predictor calls when its image classifier model generates a prediction.
+    /// - Parameter predictions: An array of predictions.
+    /// - Tag: imagePredictionHandler
+    private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+        guard let predictions = predictions else {
+            updatePredictionLabel("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        updatePredictionLabel(predictionString)
+    }
+
+    private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+            // For classifications with more than one name, keep the one before the first comma.
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
     }
 
 }
