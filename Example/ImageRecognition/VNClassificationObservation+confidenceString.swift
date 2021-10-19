bfd1085273dbb9df8dd5d249d45c26f7243eb33e
Artur Pahasian <art@devs.ae>
2021-10-19 23:00:59 +0300

    Added functionality of objects detection for selected photo from phone photos

---
+++ Example/ImageRecognition/VNClassificationObservation+confidenceString.swift
@@ -0,0 +1,28 @@
+//
+//  VNClassificationObservation+confidenceString.swift
+//  ImageRecognition
+//
+//  Created by Arthur Poghosyan on 18.10.21.
+//
+
+import Vision
+
+extension VNClassificationObservation {
+
+    var confidencePercentageString: String {
+        let percentage = confidence * 100
+
+        switch percentage {
+            case 100.0...:
+                return "100%"
+            case 10.0..<100.0:
+                return String(format: "%2.1f", percentage)
+            case 1.0..<10.0:
+                return String(format: "%2.1f", percentage)
+            case ..<1.0:
+                return String(format: "%1.2f", percentage)
+            default:
+                return String(format: "%2.1f", percentage)
+        }
+    }
+}
--- Example/ImageRecognition/ViewController.swift
+++ Example/ImageRecognition/ViewController.swift
@@ -2,23 +2,88 @@
 //  ViewController.swift
 //  ImageRecognition
 //
-//  Created by Shahen on 10/16/2021.
-//  Copyright (c) 2021 Shahen. All rights reserved.
+//  Created by Arthur Poghosyan on 14.10.21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let imagePredictor = ImagePredictor()
+
+    let predictionsToShow = 2
+
+    // MARK: Main storyboard outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+    
+    // MARK: Main storyboard actions
+    @IBAction func singleTap() {
+        present(photoPicker, animated: false)
+    }
+    
+    // MARK: Main storyboard updates
+    func updateImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updatePredictionLabel(_ message: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = message
+        }
+    }
+
+    func userSelectedPhoto(_ photo: UIImage) {
+        updateImage(photo)
+        updatePredictionLabel("Making predictions for the photo...")
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.classifyImage(photo)
+        }
+    }
+    
+    // MARK: Image prediction methods
+    /// Sends a photo to the Image Predictor to get a prediction of its content.
+    /// - Parameter image: A photo.
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+    /// The method the Image Predictor calls when its image classifier model generates a prediction.
+    /// - Parameter predictions: An array of predictions.
+    /// - Tag: imagePredictionHandler
+    private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+        guard let predictions = predictions else {
+            updatePredictionLabel("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        updatePredictionLabel(predictionString)
+    }
+
+    private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+            // For classifications with more than one name, keep the one before the first comma.
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
     }
 
 }
-
